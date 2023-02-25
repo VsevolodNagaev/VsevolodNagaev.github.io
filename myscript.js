7dabfdcba4d768d28061ec3ae77313126d152321
Vsevolod <48849178+VsevolodNagaev@users.noreply.github.com>
2023-02-25 17:31:38 +0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,22 +1,27 @@
-      var data = "Hello World"; // Replace with your actual data
-      var time = new Date().toISOString(); // Get the current time in ISO format
+const http = require('http');
 
-      // Create a JSON object with data and time properties
-      var jsonData = {
-          "data": data,
-          "time": time
-      };
+const hostname = 'https://vsevolodnagaev.github.io/index.html';
+const port = 3000;
 
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            }
-      };
-      xhttp.open("POST", "https://your-esp32-ip-address/data", true);
-      xhttp.setRequestHeader("Content-type", "application/json");
-      xhttp.send(JSON.stringify(jsonData));
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  
+  var data = "Hello World"; // Replace with your actual data
+  var time = new Date().toISOString(); // Get the current time in ISO format
 
+  // Create a JSON object with data and time properties
+  var jsonData = {
+      "data": data,
+      "time": time
+  };
 
-      // Log the JSON string to the console
-      console.log(jsonString);
+  // Convert the JSON object to a string
+  var jsonString = JSON.stringify(jsonData);
+  
+  res.end(jsonString);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
