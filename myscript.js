5ab7ce9f9024bf53fecd48b9b88a7b8fbc4c27d0
Vsevolod <48849178+VsevolodNagaev@users.noreply.github.com>
2023-02-25 17:34:49 +0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,27 +1,26 @@
 const http = require('http');
 
-const hostname = 'https://vsevolodnagaev.github.io/index.html';
-const port = 3000;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   
-  var data = "Hello World"; // Replace with your actual data
-  var time = new Date().toISOString(); // Get the current time in ISO format
-
+  // Replace this with your actual data
+  const data = "Hello World";
+  
+  // Get the current time in ISO format
+  const time = new Date().toISOString();
+  
   // Create a JSON object with data and time properties
-  var jsonData = {
-      "data": data,
-      "time": time
+  const jsonData = {
+    data,
+    time
   };
-
+  
   // Convert the JSON object to a string
-  var jsonString = JSON.stringify(jsonData);
+  const jsonString = JSON.stringify(jsonData);
   
   res.end(jsonString);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
